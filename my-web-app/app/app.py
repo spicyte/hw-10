65c420e54837b79d8f771cd840896c08e9f2d898
Kate Kovalenko <115945041+spicyte@users.noreply.github.com>
2024-12-22 19:17:20 +0300

    Add files via upload

---
+++ my-web-app/app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
